ef2e19fc9c6dacb1b553adf5fcfe6d0434859835
naeimsafaee <naeimsafaee1412@gmail.com>
2020-05-16 00:19:05 +0430

    init

--- app/Image.php
+++ app/Image.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model{
 
     protected $fillable = ["name" , "path"];
-    protected $hidden = ['created_at', 'updated_at' , 'name' , 'path'];
+    protected $hidden = ['created_at', 'updated_at' , 'name' , 'path' , 'id'];
     protected $appends = ['url'];
 
     public function getUrlAttribute(){
