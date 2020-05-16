d8dd7e18a558ee62f883dabdaca2dc32cc059fa5
naeimsafaee <naeimsafaee1412@gmail.com>
2020-05-16 12:17:21 +0430

    init

--- app/Http/Controllers/Api/GradeController.php
+++ app/Http/Controllers/Api/GradeController.php
@@ -11,45 +11,37 @@ use App\User;
 use Illuminate\Http\Request;
 use Validator;
 
-class GradeController extends Controller
-{
+class GradeController extends Controller{
     /**
      * Display a listing of the resource.
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
-    {
+    public function index(){
         $grades = Grade::all();
 
-        return response()->json(["data_count" => $grades->count() , "data" => $grades], 200);
+        return response()->json(["data_count" => $grades->count(), "data" => $grades], 200);
     }
 
     /**
      * Store a newly created resource in storage.
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
-    {
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'title' => 'required|string',
-            ],
-            [
-                "title.required" => "title is required!",
-                "title.string" => "title is not a string!",
-             ]
-        );
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+        ], [
+            "title.required" => "title is required!",
+            "title.string" => "title is not a string!",
+        ]);
 
-        if ($validator->fails()) {
-            return response()->json(
-                [
-                    "responseCode" => 401,
-                    "errorCode" => 'incomplete data',
-                    'message' => $validator->errors(),
+        if($validator->fails()){
+            return response()->json([
+                "responseCode" => 401,
+                "errorCode" => 'incomplete data',
+                'message' => $validator->errors(),
 
-                ], 401);
+            ], 401);
         }
 
         Grade::updateOrcreate([
@@ -61,34 +53,41 @@ class GradeController extends Controller
 
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id){
         //
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(Request $request, $id){
+
+        $validator = Validator::make($request->all(), [
+            'grade_title' => 'required|string',
+        ], [
+            "grade_title.required" => "grade_title is required!",
+            "grade_title.string" => "grade_title is not a string!",
+        ]);
+
+        $grade = Grade::find($id);
+        $grade->title = $request->grade_title;
+        $grade->save();
+
+        return response()->json(["success" => ["message" => "grade successfully edited!"]],200);
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
-    {
+    public function destroy($id){
         $grade = Grade::find($id);
         if($grade == null){
             return response()->json(["error" => ["message" => "grade not found!"]], 404);
