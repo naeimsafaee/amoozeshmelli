1d06dd5922e34f39485a553e45c6fccb606d9a8f
naeimsafaee <naeimsafaee1412@gmail.com>
2020-05-17 15:30:41 +0430

    init

--- app/Http/Controllers/Api/SectionController.php
+++ app/Http/Controllers/Api/SectionController.php
@@ -31,11 +31,13 @@ class SectionController extends Controller{
             ], 401);
         }
 
-        $sections = Section::where(["teacher_id" => $request->teacher_id , "subject_id" => $request->subject_id])
-            ->select("id","title" , "price" , "gift_price" , "early_price" ,"award", "pre_section_id" , "opening_date" ,"can_pass")->get();
+        $sections = Section::where([
+            "teacher_id" => $request->teacher_id,
+            "subject_id" => $request->subject_id,
+        ])->select("id", "title", "price", "gift_price", "early_price", "award", "pre_section_id", "opening_date", "can_pass")->get();
 
 
-        return response()->json(["data_count" => $sections->count() , "data" => $sections] , 200);
+        return response()->json(["data_count" => $sections->count(), "data" => $sections], 200);
     }
 
     /**
@@ -123,20 +125,87 @@ class SectionController extends Controller{
     /**
      * Display the specified resource.
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id){
-        //
+
     }
 
     /**
      * Update the specified resource in storage.
      * @param \Illuminate\Http\Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id){
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'price' => 'required|integer',
+            'gift_price' => 'required|integer',
+            'early_price' => 'required|integer',
+            'award' => 'required|integer',
+            'helper_award' => 'required|integer',
+            'teacher_id' => 'required|integer|exists:teachers,id',
+            'subject_id' => 'required|integer|exists:subjects,id',
+            'quiz_id' => 'required|integer|exists:quizzes,id',
+            'pre_section_id' => 'integer|exists:sections,id',
+            'can_pass' => 'integer|required',
+            'opening_date' => ["required", new persian_date()],
+        ], [
+            "title.required" => "title is required!",
+            "price.required" => "price is required!",
+            "gift_price.required" => "gift_price is required!",
+            "early_price.required" => "early_price is required!",
+            "award.required" => "award is required!",
+            "helper_award.required" => "helper_award is required!",
+            "teacher_id.required" => "teacher_id is required!",
+            "subject_id.required" => "subject_id is required!",
+            "quiz_id.required" => "quiz_id is required!",
+            "opening_date.required" => "opening_date is required!",
+            "teacher_id.exists" => "teacher_id does not exist!",
+            "subject_id.exists" => "subject_id does not exist!",
+            "quiz_id.exists" => "quiz_id does not exist!",
+            "pre_section_id.exists" => "pre_section_id does not exist!",
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                "responseCode" => 401,
+                "errorCode" => 'incomplete data',
+                'message' => $validator->errors(),
+
+            ], 401);
+        }
+
+        $pre_section_id = null;
+        if(isset($request->pre_section_id)){
+            $pre_section_id = $request->pre_section_id;
+        }
+
+        $section = Section::find($id);
+        if($section == null){
+            return response()->json(["error" => ["message" => "section not found!"]], 404);
+        }
+
+        $opening_date = explode("/", $request->opening_date);
+
+        $opening_date = jalali_to_gregorian($opening_date[0], $opening_date[1], $opening_date[2]);
+        $opening_date = implode("-", $opening_date);
+
+        $section->title = $request->title;
+        $section->price = $request->price;
+        $section->gift_price = $request->gift_price;
+        $section->early_price = $request->early_price;
+        $section->award = $request->award;
+        $section->helper_award = $request->helper_award;
+        $section->teacher_id = $request->teacher_id;
+        $section->subject_id = $request->subject_id;
+        $section->quiz_id = $request->quiz_id;
+        $section->pre_section_id = $pre_section_id;
+        $section->can_pass = $request->can_pass;
+        $section->opening_date = $opening_date;
+
+        return response()->json(["success" => ["message" => "section successfully edited!"]], 200);
     }
 
     /**
