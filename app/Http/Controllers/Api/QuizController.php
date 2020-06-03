0ddfb21e61688c8f80ca38ae0387af6fcd2923ed
naeimsafaee <naeimsafaee1412@gmail.com>
2020-06-04 01:50:41 +0430

    init

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -65,8 +65,7 @@ class QuizController extends Controller{
         $i = 0;
         foreach($question as $q){
             $q->image;
-            $option = $q->options;
-            $option->image;
+            $option = $q->options->image;
 
 
             $q["image_url"] = $q["image"]["url"];
