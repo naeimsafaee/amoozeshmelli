e6b6aa8165c4788b9b8206f12b5de34f79355bae
naeimsafaee <naeimsafaee1412@gmail.com>
2020-06-04 22:50:34 +0430

    init

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -84,7 +84,13 @@ class QuizController extends Controller{
 
     public function get_complete_quiz(){
 
-        $quiz_ids = Section::where("quiz_id" , "<>" , "")->get("quiz_id");
+        $sections = Section::where("quiz_id" , "<>" , "")->get();
+
+        $quiz_ids = [];
+
+        foreach($sections as $section){
+            $quiz_ids[] = $section["quiz_id"];
+        }
 
         return response()->json(["success" => $quiz_ids], 200);
 //        Quiz::whereNotIn('id', [100, 200])->get();
